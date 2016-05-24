a88258907481b6c913caec179efeba0f9250fcb1
Vivek <vivekaxl@gmail.com>
2016-05-23 21:23:27 -0400

    cleaned

--- test.py
+++ test.py
@@ -3,12 +3,20 @@ from os import listdir
 import pandas as pd
 
 data_folder = "./Data/"
-folder_names = ["AEEEM", "NASA", "PROMISE"]
+folder_names = ["PROMISE", "AEEEM", "NASA",]
 
-def run_experiment(foldername):
+def run_cleaning(foldername):
     filenames = listdir(foldername)
     for filename in filenames:
-        data = pd.read_csv(filename)
+        df = pd.read_csv(foldername + filename)
+        dependent_feature = [c for c in df.columns.tolist() if "<$" in c][-1]
+        df[dependent_feature] = df[dependent_feature].apply(lambda x: 1 if x > 0 else 0)
+
+
+
+        import pdb
+        pdb.set_trace()
+
 
 
     import pdb
@@ -17,4 +25,4 @@ def run_experiment(foldername):
 
 if __name__ == "__main__":
     for folder_name in folder_names:
-        run_experiment(data_folder + folder_name + "/")
+        run_cleaning(data_folder + folder_name + "/")
